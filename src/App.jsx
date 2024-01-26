3393071d333d7dad35d5c490e690d5b069280f40
KendraLa <kendralambillon@gmail.com>
2024-01-26 09:51:20 +0100

    Initializing exercice

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+
+export default function App() {
+  return(
+    <>
+      <h1>Primeros elementos</h1>
+      <Button variant="contained">Mi primer boton</Button>
+    </>
+  )
+}
