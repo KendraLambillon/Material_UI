f0ee96b143e31fbb395a4d9a9fd2ee62923ac371
KendraLa <kendralambillon@gmail.com>
2024-01-26 13:37:10 +0100

    Container, Typography, Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { Button } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 export default function App() {
   return(
-    <>
+    <Container maxWidth='sm'
+    sx={{ border:2, boxShadow: 3, pb: 2, m: 3}} >
       <h1>Primeros elementos</h1>
+      <Typography variant="h1" component='h2'>
+        Titulo 1
+      </Typography>
+      <Typography variant="h1" component='h3'>
+        Titulo 2
+      </Typography>
+      <Typography variant="h1" component='h1' textAlign='center' color='blue' mt={5}>
+        Titulo 3
+      </Typography>
+      <Box sx={{ border:2, m:4, p:2, bgcolor:'lightgray', borderColor:'gray'}}>
+        <Typography variant="h4" component='span'>Nueva Box</Typography>
+      </Box>
       <Button variant="contained">Mi primer boton</Button>
-    </>
+    </Container>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import './index.css'
 import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
