de08e38714813b25f54fdc0e9ce1787334a2fd98
KendraLa <kendralambillon@gmail.com>
2024-01-26 14:01:59 +0100

    Theming

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ export default function App() {
       <Typography variant="h1" component='h1' textAlign='center' color='blue' mt={5}>
         Titulo 3
       </Typography>
+      <Typography variant="h1" component='h2' textAlign='center' color='gray' mt={5}>
+        Primeros componentes
+      </Typography>
       <Box sx={{ border:2, m:4, p:2, bgcolor:'lightgray', borderColor:'gray'}}>
         <Typography variant="h4" component='span'>Nueva Box</Typography>
       </Box>
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,26 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#00ad3a',
+    },
+    secondary: {
+      main: '#ff02a5',
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/*Este es un comentario: CssBaseline --> para un reseteo del css, todo del mismo tamaño, etc  */}
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      {/*Este es un comentario: CssBaseline --> para un reseteo del css, todo del mismo tamaño, etc  */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
